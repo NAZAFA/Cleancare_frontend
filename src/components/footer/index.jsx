58b5278ca0cd0f7858882d2db8094529d5530485
ayarabia <ayarabia732@gmail.com>
2023-10-06 15:51:01 +0300

    footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -5,9 +5,9 @@ import {BiLogoFacebook ,BiLogoTwitter ,BiLogoInstagram} from 'react-icons/bi'
 function Footer() {
   return (
   <div className='bg-gray'>
-      <div className='grid md:grid-cols-3 sm:grid-cols-1  py-16 container'>
+      <div className='grid lg:grid-cols-3 grid-cols-1  py-16 container mx-auto'>
           <div className='flex justify-center   items-start'><img src={logo} className=" -mt-16 w-[15rem] h-[13rem] " alt="logo"  /></div>
-        <div className='flex justify-start p-4  lg:p-0'>
+        <div className='flex lg:justify-start justify-center p-4  lg:p-0'>
         <div className='text-xl font-normal text-left me-16'>
             <p className='text-2xl font-semibold mb-4 '>About Us</p>
             <p className='mb-3'><a href="#">About</a></p>
@@ -21,8 +21,8 @@ function Footer() {
             <p><a href="#">Contact</a></p>
           </div>
         </div>
-          <div className='px-4 py-8 lg:p-0'>
-            <p className='text-2xl font-semibold mb-4 text-left'>Newsletter</p>
+          <div className='px-4 md:px-4 py-8 lg:p-0'>
+            <p className='text-2xl font-semibold pb-4 text-left'>Newsletter</p>
              <div className='bg-white p-2 flex justify-between align-center '>
               <input type="text" placeholder='Email address' className='w-full outline-none'/>
                <img src={send} alt="sendIMage" className='w-10 h-10 block' />
@@ -35,13 +35,13 @@ function Footer() {
           </div>
        </div>
   <div className='bg-[#201C26]  '>
-  <div className='flex lg:justify-between  justify-center items-center flex-wrap  text-[#FFFFFFAB]  container mx-auto py-10'>
-     <div className='text-base font-normal text-left flex flex-wrap mb-4  lg-mb-0'>
+  <div className='flex md:justify-between  justify-center items-center flex-wrap  text-[#FFFFFFAB] md:px-8 px-3 container mx-auto py-10'>
+     <div className='text-base font-normal text-left flex flex-wrap mb-4  md:mb-0'>
            <p className='me-8'><a href="#">For Home</a></p>
             <p className='me-8'><a href="#">Category</a></p>
-            <p><a href="#">Contact</a></p>
+            <p ><a href="#">Contact</a></p>
           </div>
-      <p >copyright All Reservied @ Cleancare 2022-2023</p>
+      <p className='text-base'>copyright All Reservied @ Cleancare 2022-2023</p>
      </div>
   </div>
   </div>
